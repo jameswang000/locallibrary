e539bbacc8e39ec737b6d9c35f1151c5147780b0
jameswang000 <jameswa2@andrew.cmu.edu>
2024-10-11 16:05:57 -0400

    Added the Genre model to the catalog application

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.urls import reverse
+from django.db.models import UniqueConstraint
+from django.db.models.functions import Lower
+
 
 # Create your models here.
+class Genre(models.Model): 
+  name = models.CharField(
+    max_length=200,
+    unique=True,
+    help_text="Enter a book genre (ex. Science Fiction, Romance)"
+  )
+
+  def __str__(self): 
+    return self.name 
+  
+  def get_absolute_url(self):
+    return reverse('genre-detail', args=[str(self.id)])
+  
+  class Meta:
+    constraints = [
+      UniqueConstraint(
+        Lower('name'),
+        name='genre_name_case_insensitive_unique',
+        violation_error_message = 'Genre already exists (case insensitive match)'
+      ),
+    ]
